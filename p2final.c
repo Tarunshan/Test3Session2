5ce7ccde4dbf240c2477d58111833f0cfa6fc9e0
Tarunshan <94883107+Tarunshan@users.noreply.github.com>
2022-03-08 03:32:29 +0000

    -

--- p2final.c
+++ p2final.c
@@ -1,1 +1,17 @@
 #include<stdio.h>
+typedef struct fraction
+{
+  int num,den;
+}fraction;
+fraction input_fraction()
+{
+  int a;
+  printf("Enter numerator and denominator value ");
+  scanf("%d%d",&a.num,&a.den);
+  return a;
+}
+
+fraction largest(fraction f1,fraction f2,frcation f3)
+{
+  
+}
